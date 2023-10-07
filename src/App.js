e55b22bfd0a9b10fdc6d9dee210e9abf2081d8e6
Ali Iqbal <aliiqbal436@gmail.com>
2023-10-07 13:40:43 +0500

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import { Button } from "react-bootstrap";
+
+const socket = io("http://144.202.14.21:3007");
 
 function App() {
+  const [htmlContent, setHtmlContent] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const runSpeedTest = async () => {
+    setLoading(true);
+    socket.emit('runTest')
+  };
+
+
+  useEffect(() => {
+    socket.connect();
+    socket.on("connect", () => {
+      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    });
+    // Listen for incoming messages from the server
+    socket.on("testResult", (message) => {
+      setHtmlContent(message);
+      setLoading(false);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {htmlContent ? (
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      ) : (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "100vh" }}
         >
-          Learn React
-        </a>
-      </header>
+          <Button className="btn btn-primary" onClick={runSpeedTest}>
+            {loading ? (
+              <>
+                <span className="sr-only">
+                  Please wait for the test result{" "}
+                </span>
+                <div className="spinner-border" role="status">
+                  <span className="sr-only"></span>
+                </div>
+              </>
+            ) : (
+              "Run Speed Test"
+            )}
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
